639c7fc2e2231937b4f30a0c5afca531b5eddad0
Liam Pierce <lsp425@gmail.com>
2022-10-26 10:57:56 -0700

    Dealing with logic camera subscription.

---
+++ README.md
@@ -0,0 +1,10 @@
+Launch the competition environment:
+
+```
+roslaunch final_project_5 competition.launch
+```
+
+Launch the code that attempts the competition:
+```
+roslaunch final_project_5 compete.launch
+```
--- src/comp.cpp
+++ src/comp.cpp
@@ -1,29 +1,101 @@
+#include <map>
 
 #include "ros/ros.h"
 #include "std_srvs/Trigger.h"
 #include "std_srvs/SetBool.h"
 
-std_srvs::Trigger begin_comp;
-std_srvs::SetBool my_bool_var;
+#include "osrf_gear/Order.h"
+#include "osrf_gear/StorageUnit.h"
+#include "osrf_gear/GetMaterialLocations.h"
+#include "osrf_gear/LogicalCameraImage.h"
+
+using namespace std;
+
+
+std::vector<osrf_gear::Order> orderQueue;
+
+ros::ServiceClient materialLocationsService;
+std::map<std::string, ros::Subscriber> logicCameras;
+
+std::string * getBin(osrf_gear::Product p){
+  osrf_gear::GetMaterialLocations c;
+  c.request.material_type = p.type.c_str();
+  materialLocationsService.call(c);
+
+  if (!c.response.storage_units.size()){
+    ROS_INFO("\t\tNOT FOUND IN ANY BIN!");
+    return NULL;
+  }
+
+  return &c.response.storage_units[0].unit_id;
+}
+
+void orderHandler(const osrf_gear::Order& o){
+  ROS_INFO("Received order.");
+  orderQueue.push_back(o);
+
+  for (osrf_gear::Shipment s : o.shipments) {
+    ROS_INFO("SHIPMENT %s", s.shipment_type.c_str());
+    ROS_INFO("\tPRODUCTS:");
+
+    for (osrf_gear::Product p : s.products){
+      ROS_INFO("\t\tPRODUCT: %s", p.type.c_str());
+
+      std::string* binId = getBin(p);
+      if (binId){
+        ROS_INFO_STREAM(binId);
+      }
+    }
+  }
+}
+
+void logicalCameraHandler(const osrf_gear::LogicalCameraImage&, std::string* bin){
+
+}
 
 int main(int argc, char **argv){
 
   ros::init(argc, argv, "final_project_5");
   ros::NodeHandle n;
 
-  ros::ServiceClient begin_client = n.serviceClient<std_srvs::Trigger>("/ariac/start_competition");
-  begin_client.call(begin_comp);
+  std_srvs::Trigger beginComp;
+  ros::ServiceClient beginClient = n.serviceClient<std_srvs::Trigger>("start_competition");
 
-  if (!begin_client.call(begin_comp)){
-    ROS_ERROR("Competition service call failed!  Goodness Gracious!!");
-    return 1;
+  ROS_INFO("Sending begin client call.");
+  beginClient.call(beginComp);
+
+  if (!beginComp.response.success){
+    ROS_WARN("Competition service returned failure: %s", beginComp.response.message.c_str());
+  }else{
+    ROS_INFO("Competition service called successfully: %s", \
+    beginComp.response.message.c_str());
   }
 
-  // Sample output for service failure.
-  ROS_WARN("Competition service returned failure: %s", begin_comp.response.message.c_str());
-  // Sample output for service success.
-  ROS_INFO("Competition service called successfully: %s", \
-  begin_comp.response.message.c_str());
+  materialLocationsService = n.serviceClient<osrf_gear::GetMaterialLocations>("material_locations");
+
+  for (int i = 1;i<=6;i++){
+    std::string topic = "logical_camera_bin"+std::to_string(i);
+
+    logicCameras.insert(pair<std::string, ros::Subscriber>(
+      topic,
+      n.subscribe<osrf_gear::LogicalCameraImage>(
+        topic,
+        10,
+        boost::bind(logicalCameraHandler, _1)
+      )
+    ));
+  }
+
+  for (int i = 1;i<=2;i++){
+    //logicCameras.insert(pair<std::string, ros::Subscriber>("agv"+std::to_string(i), n.subscribe("logical_camera_agv"+std::to_string(i), 10, handleLogicCameras)));
+  }
+
+  for (int i = 1;i<=2;i++){
+    //logicCameras.insert(pair<std::string, ros::Subscriber>("qc"+std::to_string(i), n.subscribe("quality_control_sensor_"+std::to_string(i), 10, handleLogicCameras)));
+  }
+
+  ros::Subscriber orders = n.subscribe("orders", 1000, orderHandler);
+
 
   ros::Rate loop_rate(100);
 
