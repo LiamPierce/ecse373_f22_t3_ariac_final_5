1968e2c517e6b312c55fed6d2cf9c6589003425f
Liam Pierce <lsp425@gmail.com>
2022-12-09 08:11:10 -0800

    Phase 2 / 3

--- src/comp.cpp
+++ src/comp.cpp
@@ -3,6 +3,7 @@
 
 #include "ros/ros.h"
 #include "std_srvs/Trigger.h"
+#include "std_msgs/String.h"
 
 #include "sensor_msgs/JointState.h"
 #include "trajectory_msgs/JointTrajectory.h"
@@ -14,9 +15,13 @@
 #include "osrf_gear/LogicalCameraImage.h"
 #include "osrf_gear/VacuumGripperControl.h"
 #include "osrf_gear/VacuumGripperState.h"
+#include "osrf_gear/AGVControl.h"
 
 #include "tf2_ros/transform_listener.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/TransformStamped.h"
 
@@ -44,6 +49,8 @@ ros::ServiceClient gripperClient;
 osrf_gear::VacuumGripperState gripperState;
 
 ros::Publisher armPositionPublisher;
+typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> TrajectoryServer;
+TrajectoryServer* trajectoryActionServer;
 
 tf2_ros::Buffer tfBuffer;
 
@@ -52,6 +59,11 @@ double T_forward[4][4], T_destination[4][4];
 
 
 trajectory_msgs::JointTrajectory desired;
+
+std::map<string, string> agvStates = {{"agv1", "ready_to_deliver"}, {"agv2", "ready_to_deliver"}};
+ros::ServiceClient agv1Client;
+ros::ServiceClient agv2Client;
+
 
 double remapValues(double x, double in_min, double in_max, double out_min, double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -146,10 +158,10 @@ void gripperStateListener(const osrf_gear::VacuumGripperStateConstPtr& msg) {
 	gripperState = *msg;
 }
 
-geometry_msgs::TransformStamped getArmTransformFrom(std::string bin){
+tf2::Transform getArmTransformFrom(std::string bin){
   geometry_msgs::TransformStamped tf;
   try{
-    tf = tfBuffer.lookupTransform("arm1_base_link", "logical_camera_"+bin+"_frame", ros::Time(0.0), ros::Duration(1.0));
+    tf = tfBuffer.lookupTransform("arm1_base_link", bin, ros::Time(0.0), ros::Duration(1.0));
     ROS_INFO(
       "Transform to [%s] from [%s]", tf.header.frame_id.c_str(),
       tf.child_frame_id.c_str()
@@ -158,19 +170,49 @@ geometry_msgs::TransformStamped getArmTransformFrom(std::string bin){
     ROS_ERROR("Transform error: %s", ex.what());
   }
 
-  return tf;
+  tf2::Transform tf_transform;
+  tf2::fromMsg(tf.transform, tf_transform);
+
+  return tf_transform;
+}
+
+//This is an attempt to fix the tf2 transformations I've been experiencing.
+geometry_msgs::Pose applyTransform(const tf2::Transform& transform, const geometry_msgs::Pose& pose){
+
+  tf2::Transform tf_pose;
+  tf2::fromMsg(pose, tf_pose);
+
+  tf2::Transform result = transform * tf_pose;
+
+  geometry_msgs::Pose result_pose;
+  tf2::toMsg(result, result_pose);
+
+  return result_pose;
+}
+
+void agvListener(const std_msgs::StringConstPtr &msg, const string &agv) {
+  agvStates[agv] = msg->data;
 }
 
 void printPose(const geometry_msgs::Pose &pose) {
   ROS_INFO("%f %f %f", pose.position.x, pose.position.y, pose.position.z);
 }
 
+double poseToYaw(const geometry_msgs::Pose& pose){
+  tf2::Quaternion q;
+  tf2::fromMsg(pose.orientation, q);
+  tf2::Matrix3x3 mat(q);
+
+  double roll, pitch, yaw;
+  mat.getRPY(roll, pitch, yaw);
+
+  return yaw;
+}
+
 int packets = 0;
 int actionServerPackets = 0;
-void moveArm(geometry_msgs::Pose &desired){
 
-  printPose(desired);
-
+ArmJointState getKinematicsFromPose(geometry_msgs::Pose &desired, bool isAGVPose = false){
   T_forward[0][3] = desired.position.x;
   T_forward[1][3] = desired.position.y;
   T_forward[2][3] = desired.position.z;
@@ -200,10 +242,19 @@ void moveArm(geometry_msgs::Pose &desired){
   }
 
   int sol_idx = 0;
-  for (int i = 0; i < num_sols; ++i) {
-    if (output_poses[i][3] > M_PI && output_poses[i][1] > M_PI && output_poses[i][2] < M_PI) {
-      sol_idx = i;
-      break;
+  if (isAGVPose){
+    for (int i = 0; i < num_sols; ++i) {
+      if (output_poses[i][3] > M_PI && output_poses[i][1] < M_PI && output_poses[i][2] < M_PI) {
+        sol_idx = i;
+        break;
+      }
+    }
+  }else{
+    for (int i = 0; i < num_sols; ++i) {
+      if (output_poses[i][3] > M_PI && output_poses[i][1] > M_PI && output_poses[i][2] < M_PI) {
+        sol_idx = i;
+        break;
+      }
     }
   }
 
@@ -230,6 +281,9 @@ void moveArm(geometry_msgs::Pose &desired){
     ROS_INFO_STREAM("Goal joint state: " << ss.str());
   }
 
+  return goalJointState;
+}
+void moveToGoalJointState(ArmJointState goalJointState, ros::Duration duration){
   trajectory_msgs::JointTrajectory jointTrajectory;
 
   jointTrajectory.header.seq = packets++;
@@ -241,7 +295,7 @@ void moveArm(geometry_msgs::Pose &desired){
   jointTrajectory.points.resize(2);
 
   jointTrajectory.points[0].time_from_start = ros::Duration(0.1);
-  jointTrajectory.points[1].time_from_start = ros::Duration(2.1);
+  jointTrajectory.points[1].time_from_start = duration;
 
   for (int i = 0; i < 7; ++i){
     jointTrajectory.points[0].positions.push_back(jointState[i]);
@@ -265,28 +319,28 @@ void moveArm(geometry_msgs::Pose &desired){
   }
 
   armPositionPublisher.publish(jointTrajectory);
-  /*control_msgs::FollowJointTrajectoryAction jointTrajectoryPayload;
+}
 
-  jointTrajectoryPayload.action_goal.goal.trajectory = jointTrajectory;
-  jointTrajectoryPayload.action_goal.header.seq = actionServerPackets++;
-  jointTrajectoryPayload.action_goal.header.stamp = ros::Time::now();
-  jointTrajectoryPayload.action_goal.header.frame_id = "/world";
+void yawEffector(double rads){
+  ArmJointState goalRotation = jointState;
 
-  jointTrajectoryPayload.action_goal.goal_id.stamp = ros::Time::now();
-  jointTrajectoryPayload.action_goal.goal_id.id = std::to_string(actionServerPackets - 1);
+  while (rads > 2 * M_PI || rads < 0){
+    rads += (rads > 2 * M_PI ? -1 : 1) * 2 * M_PI;
+  }
 
-  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> trajectoryActionServer("arm1/arm/follow_joint_trajectory", true);
-  trajectoryActionServer.sendGoal(jointTrajectoryPayload.action_goal.goal);
-  trajectoryActionServer.waitForServer();
-  bool succeeded = trajectoryActionServer.waitForResult(ros::Duration(5.0));
+  goalRotation[6] = rads;
 
-  if (succeeded){
-    actionlib::SimpleClientGoalState state = trajectoryActionServer.getState();
-    ROS_INFO("Trajectory finished with state %s with text: %s", state.toString().c_str(), state.getText().c_str());
-  }else{
-    ROS_ERROR("TRAJECTORY TIMEOUT!");
-  }*/
-  ros::Duration(2.0).sleep();
+  moveToGoalJointState(goalRotation, ros::Duration(1));
+}
+
+void moveArm(geometry_msgs::Pose &desired, bool isAGVPose = false){
+
+  printPose(desired);
+
+  ArmJointState goalJointState = getKinematicsFromPose(desired, isAGVPose);
+  moveToGoalJointState(goalJointState, ros::Duration(3));
+
+  ros::Duration(3.2).sleep();
 }
 
 void moveBase(double to, bool absolute = false){
@@ -304,7 +358,7 @@ void moveBase(double to, bool absolute = false){
   jointTrajectory.points[1].positions.resize(jointTrajectory.joint_names.size());
 
   jointTrajectory.points[0].time_from_start = ros::Duration(0.0);
-  jointTrajectory.points[1].time_from_start = ros::Duration(2.0);
+  jointTrajectory.points[1].time_from_start = ros::Duration(1.6);
 
   for (int indy = 0; indy < 7; indy++) {
     jointTrajectory.points[1].positions[indy] = jointState[indy];
@@ -313,11 +367,28 @@ void moveBase(double to, bool absolute = false){
   jointTrajectory.points[1].positions[0] = (!absolute ? jointState[1] : 0) + to;
 
   armPositionPublisher.publish(jointTrajectory);
-  ROS_INFO("Published new joint trajectory for base.");
 
-  ros::Duration(3.0).sleep();
+  /*control_msgs::FollowJointTrajectoryAction jointTrajectoryPayload;
 
-  ROS_INFO("Done moving base.");
+  jointTrajectoryPayload.action_goal.goal.trajectory = jointTrajectory;
+  jointTrajectoryPayload.action_goal.header.seq = actionServerPackets++;
+  jointTrajectoryPayload.action_goal.header.stamp = ros::Time::now();
+  jointTrajectoryPayload.action_goal.header.frame_id = "/world";
+
+  jointTrajectoryPayload.action_goal.goal_id.stamp = ros::Time::now();
+  jointTrajectoryPayload.action_goal.goal_id.id = std::to_string(actionServerPackets - 1);
+
+  trajectoryActionServer->sendGoal(jointTrajectoryPayload.action_goal.goal);
+  trajectoryActionServer->waitForServer();
+  bool succeeded = trajectoryActionServer->waitForResult(ros::Duration(20.0));
+
+  if (succeeded){
+    actionlib::SimpleClientGoalState state = trajectoryActionServer->getState();
+    ROS_INFO("Trajectory finished with state %s with text: %s", state.toString().c_str(), state.getText().c_str());
+  }else{
+    ROS_ERROR("TRAJECTORY TIMEOUT!");
+  }*/
+  ros::Duration(4.0).sleep();
 }
 
 int main(int argc, char **argv){
@@ -368,6 +439,19 @@ int main(int argc, char **argv){
   ros::Subscriber jointStateubsriber = n.subscribe("arm1/joint_states", 1, jointStateListener);
   ros::Subscriber gripperStateSubscriber = n.subscribe("arm1/gripper/state", 1000, gripperStateListener);
 
+  for (int i = 1; i<=2;i++){
+    ros::Subscriber agvSubscriber = n.subscribe<std_msgs::String>(
+      "agv/"+std::to_string(i)+"/state",
+      32,
+      boost::bind(agvListener, _1, "agv"+std::to_string(i))
+    );
+  }
+
+  agv1Client = n.serviceClient<osrf_gear::AGVControl>("/ariac/agv1");
+  agv2Client = n.serviceClient<osrf_gear::AGVControl>("/ariac/agv2");
+  agv1Client.waitForExistence(ros::Duration(0.0));
+  agv2Client.waitForExistence(ros::Duration(0.0));
+
   armPositionPublisher = n.advertise<trajectory_msgs::JointTrajectory>("arm1/arm/command", 10);
   gripperClient = n.serviceClient<osrf_gear::VacuumGripperControl>("arm1/gripper/control");
 
@@ -375,12 +459,19 @@ int main(int argc, char **argv){
   spinner.start();
   ros::Rate sleeper(20);
 
+
+  trajectoryActionServer = new TrajectoryServer("/ariac/arm1/arm/follow_joint_trajectory", false);
+  ROS_INFO("Waiting for trajectory action server");
+  trajectoryActionServer->waitForServer();
+  ROS_INFO("Trajectory server running");
+
   bool start = true;
   geometry_msgs::Pose startPose;
   startPose.position.x = -0.05;
   startPose.position.y = 0.2;
   startPose.position.z = 0.20;
 
+  int ordersCompleted = 0;
   int count = 0;
   while (ros::ok()){
 
@@ -399,103 +490,144 @@ int main(int argc, char **argv){
       ros::Duration(2).sleep();
     }*/
 
+    if (ordersCompleted == 2){
+      return 0;
+      /*
+      osrf_gear::competition_state end;
+      end.request.state = osrf_gear::competition_state::STOP;
+      if (competitionStateClient.call(end)){
+        ROS_INFO("Successfully completed the competition");
+      }else{
+        ROS_ERROR("Failed to end the competition!");
+      }
+      */
+    }
+
     for (osrf_gear::Order o : oqc){
       if (start){
         start = false;
-        ROS_INFO("Moving arm to home.");
         moveArm(startPose);
-        //ros::shutdown();
       }
-
 
       for (osrf_gear::Shipment s : o.shipments) {
         ROS_INFO("SHIPMENT %s", s.shipment_type.c_str());
         ROS_INFO("\tPRODUCTS:");
 
         for (osrf_gear::Product p : s.products){
-          int idx = 0;
-          std::vector<std::string> bins = {"bin4", "bin5", "bin6"};
-          while (true){
-            ROS_INFO("\t\tPRODUCT: %s", p.type.c_str());
+          ROS_INFO("\t\tPRODUCT: %s", p.type.c_str());
 
-            string binId = getBinCheat(p);
-            binId = bins[floor((double) ((idx%9)) / 3)];
-            ROS_INFO_STREAM("\t\tBIN: "<<binId);
+          string binId = getBinCheat(p);
+          ROS_INFO_STREAM("\t\tBIN: "<<binId);
 
-            ROS_INFO_STREAM("\t\tLooking at logical_camera_"+(binId));
-            if (logicalCameraImages.find("logical_camera_"+(binId)) == logicalCameraImages.end()){
-              ROS_INFO("NO DATA ON THIS BIN");
-              continue;
-            }
-
-            osrf_gear::LogicalCameraImage im = logicalCameraImages["logical_camera_"+(binId)];
-            if (im.models.size() == 0){
-              ROS_ERROR("\t\tProduct not found in bin.");
-              continue;
-            }
-
-            moveBase(binPositions[binId], true);
-
-            geometry_msgs::Pose partPose, goalPose;
-            geometry_msgs::Pose cameraPose, emptyPose, correctionPose;
-
-            geometry_msgs::TransformStamped tf = getArmTransformFrom(binId);
-
-            cameraPose = im.pose;
-
-            partPose = im.models[(idx)%3].pose;
-            goalPose.position.x = 0;
-            goalPose.position.y = 0;
-            goalPose.position.z = 0;
-
-            idx = idx + 1;
-
-            tf2::doTransform(partPose, goalPose, tf);
-            tf2::doTransform(emptyPose, correctionPose, tf);
-
-            ROS_INFO("Part Pose: ");
-            printPose(partPose);
-            ROS_INFO("ARM Frame Pose: ");
-            printPose(goalPose);
-
-            goalPose.position.x = remapValues(goalPose.position.x, -0.397151, -0.804914, -0.21012, -0.377455);
-            goalPose.position.y = remapValues(goalPose.position.y, 0.435990, 0.881735, 0.1455, 0.356278);
-            goalPose.position.z += 0.24;
-            goalPose.orientation.w = 0.707;
-            goalPose.orientation.x = 0.0;
-            goalPose.orientation.y = 0.707;
-            goalPose.orientation.z = 0.0;
-
-            osrf_gear::VacuumGripperControl request;
-            request.request.enable = true;
-  		      bool success = gripperClient.call(request);
-            if (!success){
-              ROS_ERROR("GRIPPER FAILED TO ENABLE");
-            }
-
-            moveArm(goalPose);
-
-            ros::Duration(1).sleep();
-
-            moveArm(startPose);
-            ros::Duration(1).sleep();
-            moveArm(goalPose);
-
-            ros::Duration(1).sleep();
-
-            request.request.enable = false;
-  		      success = gripperClient.call(request);
-            if (!success){
-              ROS_ERROR("GRIPPER FAILED TO DISABLE");
-            }
-
-            moveArm(startPose);
-            ros::Duration(1).sleep();
+          ROS_INFO_STREAM("\t\tLooking at logical_camera_"+(binId));
+          if (logicalCameraImages.find("logical_camera_"+(binId)) == logicalCameraImages.end()){
+            ROS_INFO("NO DATA ON THIS BIN");
+            continue;
           }
+
+          osrf_gear::LogicalCameraImage im = logicalCameraImages["logical_camera_"+(binId)];
+          if (im.models.size() == 0){
+            ROS_ERROR("\t\tProduct not found in bin.");
+            continue;
+          }
+
+          moveBase(binPositions[binId], true);
+
+          ros::Duration(0.5).sleep(); //Make sure the arm isn't oscillating from the base movement.
+
+          tf2::Transform tf = getArmTransformFrom("logical_camera_"+binId+"_frame");
+          geometry_msgs::Pose goalPose = applyTransform(tf, im.models.front().pose);
+
+          ROS_INFO("Part Pose: ");
+          printPose(im.models.front().pose);
+          ROS_INFO("ARM Frame Pose: ");
+          printPose(goalPose);
+
+          goalPose.position.x = remapValues(goalPose.position.x, -0.397151, -0.804914, -0.21012, -0.377455);
+          goalPose.position.y = remapValues(goalPose.position.y, 0.435990, 0.881735, 0.144, 0.358278);
+          goalPose.position.z += 0.239;
+          goalPose.orientation.w = 0.707;
+          goalPose.orientation.x = 0.0;
+          goalPose.orientation.y = 0.707;
+          goalPose.orientation.z = 0.0;
+
+          osrf_gear::VacuumGripperControl request;
+          request.request.enable = true;
+		      bool success = gripperClient.call(request);
+          if (!success){
+            ROS_ERROR("GRIPPER FAILED TO ENABLE");
+          }
+
+          moveArm(goalPose);
+
+          ros::Duration(1).sleep();
+
+          int agv = (s.agv_id == "agv2" || s.agv_id == "any" ? 2 : 1);
+          //agv = 1;
+
+          moveArm(startPose);
+          moveBase(agv == 1 ? 2.2 : -2.2, true);
+
+          ros::Duration(1).sleep();
+
+          tf = getArmTransformFrom("kit_tray_" + std::to_string(agv));
+          goalPose= applyTransform(tf, p.pose);
+
+          ROS_INFO("AGV Pose Request: ");
+          printPose(p.pose);
+          ROS_INFO("ARM Frame Pose: ");
+          printPose(goalPose);
+          if (agv == 1){
+            //goalPose.position.y = remapValues(goalPose.position.y, 0.519752, 0.854737, 0.2, 0.4);
+            while (ros::ok() && agvStates["agv1"] != "ready_to_deliver"){
+              ros::Duration(0.1).sleep();
+            }
+          }else{
+            //goalPose.position.y = remapValues(goalPose.position.y, -0.819752, -1.13, -0.3, -0.5);
+            while (ros::ok() && agvStates["agv2"] != "ready_to_deliver"){
+              ros::Duration(0.1).sleep();
+            }
+          }
+
+          double yaw = poseToYaw(goalPose);
+
+          goalPose.position.z = -0.2;
+          moveArm(goalPose, true);
+
+          ROS_INFO("Rotating to yaw %f", yaw);
+          yawEffector(yaw);
+
+          ros::Duration(1).sleep();
+
+          request.request.enable = false;
+		      success = gripperClient.call(request);
+          if (!success){
+            ROS_ERROR("GRIPPER FAILED TO DISABLE");
+          }
+
+          moveArm(startPose);
+          ros::Duration(1).sleep();
+        }
+
+        osrf_gear::AGVControl submit;
+        submit.request.shipment_type = s.shipment_type;
+
+        bool success = false;
+        if (s.agv_id == "agv1"){
+          success = agv1Client.call(submit);
+        }else{
+          success = agv2Client.call(submit);
+        }
+
+        if (!success){
+          ROS_ERROR("Failed to called AGV Client!");
+        }else if (!submit.response.success){
+          ROS_ERROR("Shipment unsuccessful with message %s", submit.response.message.c_str());
+        }else{
+          ROS_INFO("Successfully submitted shipment.");
         }
       }
-      ROS_INFO("Moving arm to home.");
-      moveArm(startPose);
+      ordersCompleted++;
     }
 
 
